71314d07dae6b8117333b131980fd0aab186c804
Svetlana Radisic <svetlana_radisic@yahoo.com>
2023-08-16 16:37:52 +0200

    Add file download time estimation program

---
+++ src/main/java/d15_08_2023/vezbanjeSlack3.java
@@ -0,0 +1,16 @@
+package d15_08_2023;
+
+public class vezbanjeSlack3 {
+    public static void main(String[] args) {
+        String fileName = "profile-image.log";
+        int fileSize = 25;
+        int downloadSpeed = 3;
+
+        int estimatedTime = fileSize/downloadSpeed;
+
+        System.out.println("File: "+fileName);
+        System.out.println("Size: "+fileSize+"Mb");
+        System.out.println("Download speed: "+downloadSpeed+"Mb/s");
+        System.out.println("Estimated time: "+estimatedTime+"s");
+    }
+}
